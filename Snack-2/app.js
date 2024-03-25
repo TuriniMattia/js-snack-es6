8046fe890f0c6e4dc3236902f7c46e3637e2767c
TuriniMattia <turinimattia96@gmail.com>
2024-03-25 18:17:26 +0100

    [add]snack-2

--- Snack-2/app.js
+++ Snack-2/app.js
@@ -3,37 +3,70 @@ const students = [
         id : 213,
         name : "Marco della Rovere",    
         grades : 78,
-    }
+    },
+    {
+        id : 110,
+        name : "Paola Cortellessa",    
+        grades : 96,
+    },
+    {
+        id : 250,
+        name : "Andrea Mantegna",    
+        grades : 48,
+    },
+    {
+        id : 145,
+        name : " Gaia Borromini",    
+        grades : 74,
+    },
     {
         id : 213,
-        name : "Marco della Rovere",    
-        grades : 78,
-    }
+        name : "Luigi Grimaldello",    
+        grades : 68,
+    },
     {
-        id : 213,
-        name : "Marco della Rovere",    
-        grades : 78,
-    }
+        id : 102,
+        name : " Piero della Francesca ",    
+        grades : 50,
+    },
     {
-        id : 213,
-        name : "Marco della Rovere",    
-        grades : 78,
-    }
-    {
-        id : 213,
-        name : "Marco della Rovere",    
-        grades : 78,
-    }
-    {
-        id : 213,
-        name : "Marco della Rovere",    
-        grades : 78,
-    }
-    {
-        id : 213,
-        name : "Marco della Rovere",    
-        grades : 78,
-    }
+        id : 120,
+        name : "Francesca da Polenta",    
+        grades : 84,
+    },
 ]
 
-const f
+const student = {
+    id : 213,
+    name : "Marco della Rovere",    
+    grades : 78,
+}
+
+console.log(students)
+
+const formattedStudents = []
+
+for(let i = 0; i < students.length; i++){
+    const name = students[i].name
+    const id = students[i].id
+    const grades = students[i].grades
+
+    const student = {
+        id : id,
+        name : name.toUpperCase(),
+        grades : grades
+    }
+    formattedStudents.push(student)
+}
+console.log(formattedStudents)
+
+const topStupends = []
+
+for (let i = 0; i < formattedStudents.length; i++ ){
+    const student = formattedStudents[i]
+
+    if(student.grades > 70){
+        topStupends.push(student)
+    }
+}
+console.log(topStupends)
